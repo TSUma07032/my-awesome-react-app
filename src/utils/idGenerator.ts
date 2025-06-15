43866739c1f6147dc46f39059f409f78bae8ca91
Tsubasa MORIYA <m2210643@gl.cc.uec.ac.jp>
2025-06-15 10:48:40 +0900

    2025/06/14までの進捗

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -3,15 +3,6 @@
  * @auther 守屋翼
  */
 
-// globalThis.crypto をモックする
-Object.defineProperty(globalThis, 'crypto', {
-  value: {
-    randomUUID: () => 'mock-uuid-' + Math.random().toString(36).substring(2, 15),
-  },
-});
-
-
-
 /**
  * IDGeneratorは、ユニークなIDを生成するためのクラスです。
  * 
