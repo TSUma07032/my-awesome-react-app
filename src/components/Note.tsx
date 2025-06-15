37c51c8a5621abba35173d434120271e7e39adb6
Tsubasa MORIYA <m2210643@gl.cc.uec.ac.jp>
2025-06-15 11:26:39 +0900

    noteコンポーネント完成

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -8,3 +8,35 @@ import React from 'react';
  */
 
 
+/**
+ * NotePropsは、Noteコンポーネントに渡されるプロパティの型定義です。
+ * @property {string} id - ノートのユニークなID。
+ * @property {string} text - ノートの内容。
+ * @example
+ * const noteProps: NoteProps = {
+ *   id: '12345',
+ *  text: 'This is a note.'
+ * }
+ */
+type NoteProps = {
+    id: string;
+    text: string;
+}
+
+/**
+ * Note関数コンポーネントは、ノートの内容を表示するためのReactコンポーネントです。
+ * @param {NoteProps} props - Noteコンポーネントに渡されるプロパティ。
+ * @return {JSX.Element} - ノートの内容を表示するReact要素。
+ * 
+ * @example
+ * <Note id="12345" text="This is a note." />
+ */
+export default function Note(props: NoteProps){
+    return (
+        <div className="note">
+            <h2 className="note-id">ID: {props.id}</h2>
+            <p className="note-text">{props.text}</p>
+        </div>
+    );
+}
+
