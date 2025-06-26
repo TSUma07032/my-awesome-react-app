2a473d4a39674c73a830746bf4b5b1600a24637b
Tsubasa MORIYA <m2210643@gl.cc.uec.ac.jp>
2025-06-27 08:38:45 +0900

    一旦完成

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,8 +7,12 @@
  * @typedef {object} NoteData
  * @property {string} id - 付箋のユニークなID。
  * @property {string} text - 付箋の内容。
+ * @property {number} x - 付箋のX座標。
+ * @property {number} y - 付箋のY座標。
  */
 export type NoteData = {
   id: string;
   text: string;
+  x: number; // 付箋のX座標
+  y: number; // 付箋のY座標
 };
